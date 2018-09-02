1a4cf06a9622c88c7c006936eddc6949a99b2a2f
Yannick Arndt <arndt.yannick@gmail.com>
2018-09-02 19:51:42 +0200

    Fixes?

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -19,7 +19,7 @@ public class CameraController : MonoBehaviour {
         transform.position = new Vector3(Target.transform.position.x, transform.position.y, -10);
 
         if (Mathf.Abs(transform.position.y - Target.transform.position.y) > 1)
-            transform.position = Vector3.Lerp(transform.position, Target.transform.position, 1f * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, Target.transform.position, 5f * Time.deltaTime);
 
         if (transform.position.y < -1)
             transform.position = new Vector3(transform.position.x, -1, -10);
